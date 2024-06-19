b84f19836ae39ce0386cdfdd8859a26e9f3e9174
Oscar Ventura <ovenor11@proton.me>
2024-06-19 13:35:10 +0200

    clippy images

--- modules/items/item-preview.tsx
+++ modules/items/item-preview.tsx
@@ -6,8 +6,8 @@ interface Props {
 
 export const ItemPreview = ({ item }: Props) => {
   return (
-    <div key={item.id}>
-      <img src={item.cover} alt={item.title} className="w-[300px] rounded-lg" />
+    <div key={item.id} className="m-4">
+      <img src={item.cover} alt={item.title} className="w-[300px]" />
       <p>{item.title}</p>
     </div>
   );
