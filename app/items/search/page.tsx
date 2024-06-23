50a16032a96241f9381a3ae74410f83cab6fa69f
Oscar Ventura <ovenor11@proton.me>
2024-06-23 12:38:09 +0200

    wip - search items

--- app/items/search/page.tsx
+++ app/items/search/page.tsx
@@ -1,9 +1,39 @@
+import { SubmitButton } from '@/app/login/submit-button';
 import { BackButton, Input, LucideIcons } from '@/modules';
 
-import { SearchFormContent } from '@/content';
+import { PATHS, SearchFormContent } from '@/content';
+import { createClientServer } from '@/utils';
+import { redirect } from 'next/navigation';
 
-export default function SearchItemPage() {
+export default function SearchItemPage({
+  searchParams,
+}: {
+  searchParams: { message: string };
+}) {
+  const { search } = PATHS;
   const { name, placeholder } = SearchFormContent;
+
+  const searchItem = async (formData: FormData) => {
+    'use server';
+
+    const word = formData.get(name) as string;
+    const supabase = createClientServer();
+
+    const { data, error } = await supabase
+      .from('items')
+      .select()
+      .textSearch('title', word);
+
+    if (error) {
+      return redirect(`${search}?message=No item found for "${word}"`);
+    }
+
+    if (!data.length) {
+      return redirect(`${search}?message=No item found for "${word}"`);
+    }
+
+    return redirect(`/items/${data[0].slug}`);
+  };
 
   return (
     <div className="grid w-full h-full gap-2 px-8 place-content-center place-items-center sm:max-w-md">
@@ -12,13 +42,20 @@ export default function SearchItemPage() {
         <BackButton />
         <div className="flex gap-2">
           <Input name={name} placeholder={placeholder} required />{' '}
-          <button
+          <SubmitButton
+            formAction={searchItem}
             className="hover:text-[var(--primary)] transition-all"
             type="submit"
+            pendingText="Searching... Seek and Destroy!"
           >
             <LucideIcons.search />
-          </button>
+          </SubmitButton>
         </div>
+        {searchParams?.message && (
+          <p className="p-4 mt-4 text-center bg-foreground/10">
+            {searchParams.message}
+          </p>
+        )}
       </form>
     </div>
   );
